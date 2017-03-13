17c91f0f141e099f5d0077af907f0a1e18ef35e0
PeterMazzoli <Peter.mazzoli@gmail.com>
2017-03-13 12:26:20 -0400

    add db

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,13 @@
-schema.sql
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	date TIMESTAMP not null default CURRENT_TIMESTAMP,
+	PRIMARY KEY (id)
+);
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,3 @@
-seeds.sql
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE, CURRENT_TIMESTAMP);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Breakfast Burger', FALSE, CURRENT_TIMESTAMP);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE, CURRENT_TIMESTAMP);
