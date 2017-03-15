42b361d3c58a3d27365e186450df9e8534412860
PeterMazzoli <Peter.mazzoli@gmail.com>
2017-03-15 11:20:52 -0400

    final edits

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var exphbs = require('express-handlebars');
 
 // Sets up the Express App
 var app = express();
-// var PORT = process.env.PORT || 8080;
+
 var port = 3000;
 
 app.use(express.static(__dirname + '/public'));
